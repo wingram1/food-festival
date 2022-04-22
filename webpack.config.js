8885ed5e43fcc18866478660a2511c5fd9a4cfa3
wingram1 <waingram96@gmail.com>
2022-04-21 21:19:32 -0500

    added manifest

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require("webpack");
 const path = require("path");
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 module.exports = {
   entry: {
@@ -44,6 +45,23 @@ module.exports = {
     new BundleAnalyzerPlugin({
       // outputs report to an HTML file in the dist folder
       analyzerMode: "static",
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false,
+      inject: false,
+      icons: [
+        {
+          src: path.resolve("assets/img/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
     }),
   ],
   mode: "development",
