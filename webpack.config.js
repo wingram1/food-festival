4c0bbf945fb3fc7732ebe26af82611dde805d4df
wingram1 <waingram96@gmail.com>
2022-04-21 20:53:53 -0500

    got webpack dev server working

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const webpack = require("webpack");
+const path = require("path");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/schedule.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: `${__dirname}/dist`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name(file) {
+                return "[path][name].[ext]";
+              },
+              publicPath: function (url) {
+                return url.replace("../", "/assets");
+              },
+            },
+          },
+          { loader: "image-webpack-loader" },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+    new BundleAnalyzerPlugin({
+      // outputs report to an HTML file in the dist folder
+      analyzerMode: "static",
+    }),
+  ],
+  mode: "development",
+  devServer: {
+    historyApiFallback: true,
+  },
+};
